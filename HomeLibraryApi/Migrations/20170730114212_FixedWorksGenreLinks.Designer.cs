91d9c0d9eab5728a0fa013ccd52b9f4aaaad4cea
Andrey Sergeev <a.sergeev.official@gmail.com>
2017-07-30 15:43:52 +0400

    Migration: Fixed Work's Genre Links

--- HomeLibraryApi/Migrations/20170730114212_FixedWorksGenreLinks.Designer.cs
+++ HomeLibraryApi/Migrations/20170730114212_FixedWorksGenreLinks.Designer.cs
@@ -8,9 +8,10 @@ using HomeLibraryApi.Models;
 namespace HomeLibraryApi.Migrations
 {
     [DbContext(typeof(HomeLibraryContext))]
-    partial class HomeLibraryContextModelSnapshot : ModelSnapshot
+    [Migration("20170730114212_FixedWorksGenreLinks")]
+    partial class FixedWorksGenreLinks
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.2")

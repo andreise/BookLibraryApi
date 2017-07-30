3fc9848dee88e5af0abc0cd6570034f4b9796ce0
Andrey Sergeev <a.sergeev.official@gmail.com>
2017-07-30 20:05:32 +0400

    BasicRepository

---
+++ BookLibraryApi.Repositories/BasicRepository.cs
@@ -0,0 +1,12 @@
+﻿using Common.EntityFrameworkCore.Extensions;
+using BookLibraryApi.Models;
+
+namespace BookLibraryApi.Repositories
+{
+    public sealed class BasicRepository<TEntity> : Repository<TEntity> where TEntity : class
+    {
+        public BasicRepository(BookLibraryContext context) : base(context)
+        {
+        }
+    }
+}

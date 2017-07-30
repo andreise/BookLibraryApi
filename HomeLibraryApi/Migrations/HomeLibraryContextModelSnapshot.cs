0e41c9e40a157c7273fc495c546baaab11ea5462
Andrey Sergeev <a.sergeev.official@gmail.com>
2017-07-30 10:02:08 +0400

    Add-Migration Initial

---
+++ HomeLibraryApi/Migrations/HomeLibraryContextModelSnapshot.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using HomeLibraryApi.Models;
+
+namespace HomeLibraryApi.Migrations
+{
+    [DbContext(typeof(HomeLibraryContext))]
+    partial class HomeLibraryContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ChangeDetector.SkipDetectChanges", "true")
+                .HasAnnotation("ProductVersion", "1.1.2")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+        }
+    }
+}

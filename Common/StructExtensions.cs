4e3230174dff86cc31c3f68ea053d68f8b660923
Andrey Sergeev <a.sergeev.official@gmail.com>
2017-08-17 21:06:57 +0400

    Updated StructExtensions

--- Common/StructExtensions.cs
+++ Common/StructExtensions.cs
@@ -2,6 +2,6 @@
 {
     public static class StructExtensions
     {
-        public static TStruct? AsNullable<TStruct>(this TStruct value) where TStruct : struct => (TStruct?)value;
+        public static TStruct? AsNullable<TStruct>(this TStruct value) where TStruct : struct => value;
     }
 }

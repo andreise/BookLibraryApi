46df9aff82c5a10e92cfb1d22f0e87170c66fe91
Andrey Sergeev <a.sergeev.official@gmail.com>
2018-03-03 17:31:42 +0400

    Add StringExtensions

---
+++ Common/StringExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace Common
+{
+    public static class StringExtensions
+    {
+        public static bool IsNullOrEmpty(this string value) => string.IsNullOrEmpty(value);
+
+        public static bool IsNullOrWhiteSpace(this string value) => string.IsNullOrWhiteSpace(value);
+
+        public static string OrEmpty(this string value) => value ?? string.Empty;
+    }
+}
